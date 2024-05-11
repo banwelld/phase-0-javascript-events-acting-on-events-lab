2aefd230627964162f5569f3c8773b027ec7857e
Dave Banwell <dave@davebanwell.com>
2024-05-11 18:34:42 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code here
+const findLeftPos = () => parseInt(dodger.style.left.replace('px', ''), 10);
+
+const moveDodgerLeft = () => {if (findLeftPos() > 0) dodger.style.left = `${findLeftPos() - 1}px`}
+const moveDodgerRight = () => {if (findLeftPos() < 360) dodger.style.left = `${findLeftPos() + 1}px`}
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowLeft') moveDodgerLeft();
+    if (e.key === 'ArrowRight') moveDodgerRight();
+})
